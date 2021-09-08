a872275b7eb23cf40fc8e12c17ce0d4fbd8123e1
Carlos Gonzalez <fudycg87@gmail.com>
2021-09-08 18:12:05 -0300

    Fixed player kill tracker

--- src/main/java/pgmst/pgmst/PlayerKillTracker.java
+++ src/main/java/pgmst/pgmst/PlayerKillTracker.java
@@ -1,6 +1,7 @@
 package pgmst.pgmst;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -9,10 +10,15 @@ public class PlayerKillTracker implements Listener {
     @EventHandler
     public void onPlayerKill(PlayerDeathEvent event) {
 
-        String KilledName = event.getEntity().getName();
-        String KillerName = event.getEntity().getKiller().getName();
+        Player Killer = event.getEntity().getKiller();
 
+        if (Killer != null) {
+            String KilledName = event.getEntity().getName();
+            String KillerName = Killer.getName();
 
-        Bukkit.broadcastMessage(KillerName + " Ha matado a " + KilledName + " LOLOLOLOLO");
+            Bukkit.broadcastMessage(KillerName + " Ha matado a " + KilledName + " LOLOLOLOLO");
+        }else{
+            Bukkit.broadcastMessage("Environment death");
+        }
     }
 }
